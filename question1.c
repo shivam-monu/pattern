b95d82bf9099feec854a620500e2bef002a6054a
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-30 21:43:07 +0530

    print the pattern number

---
+++ question1.c
@@ -0,0 +1,19 @@
+/*
+12345
+12345
+12345
+12345
+12345
+*/
+#include <stdio.h>
+
+int main() {
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+        printf("%d",j);
+        }
+    printf("\n");
+    }
+    return 0;
+}
