6e1891d512bd0cbb28a678ddeaf1bb6f39e98ac8
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-31 00:04:47 +0530

    print the pattern 1234 123 12 1

---
+++ question8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+1234
+123
+12
+1*/
+
+int main() {
+    int n;
+    printf("enter the row:");
+    scanf("%d",&n);
+    for (int i = 1; i <=n; i++)
+    {
+        for (int j = 1; j <= n+1-i; j++)
+        {
+            printf("%d",j);
+        }
+        
+        printf("\n");
+    }
+    
+    return 0;
+}
