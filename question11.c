a2f3cd10211db08a2c433e18140d8a2f032d6260
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-09-02 00:15:09 +0530

    print the alphabet square pattern A B C D A B C D A B C D A B C D

---
+++ question11.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/*
+A B C D
+A B C D
+A B C D
+A B C D
+
+ */
+int main() {
+    int n;
+    printf("enter the row:");
+    scanf("%d",&n);
+    for (int i = 1; i <= n; i++)
+    {
+        int a=1;
+        for (int j = 1; j <=n; j++)
+        {
+            int d=a+64;
+            char ch=(char)d;
+            
+            printf("%c ",ch);
+            a++;
+            
+        }
+        printf("\n");
+        
+        
+    }
+    
+
+    return 0;
+}
