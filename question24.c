588f6a6621200dd6d86a51a03bd28407075f2525
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-09-05 11:30:15 +0530

    triangle pattern of symbol star

---
+++ question24.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("enter the row:");
+    scanf("%d",&n);
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+        for(j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+            for ( j = 1; j <=i; j++)
+            {
+            char ch = 'A' + (j - 1);
+            printf("%c", ch);
+        }
+            
+        
+        printf("\n");
+    }
+
+}
