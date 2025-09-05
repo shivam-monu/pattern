c8057ae0a3da7d4e9fb79da5828cd999f3c6eba0
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-09-05 23:13:20 +0530

    first print the space triangle  then print the reverse triangle

---
+++ question33.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/*
+****
+ ***
+  **
+   *
+*/
+
+int main()
+{
+    int n;
+    printf("enter the row:");
+    scanf("%d", &n);
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 1; j <=i; j++)
+        {
+            printf(" ");
+        }
+        
+
+        for (int j = 1; j <= n-i; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+}
