9d07c99052ba97ad65ee3bb94fa99a199cf46725
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-09-05 16:25:17 +0530

    number pyramid

---
+++ question27.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+/*
+   1 
+  1 2 3
+ 1 2 3 4 5
+1 2 3 4 5 6 7
+*/
+
+int main()
+{
+    int n = 4;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
