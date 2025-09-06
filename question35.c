de77b4604d9db1fb63ff177942fd8e87e13eb6a6
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-09-06 22:06:43 +0530

    pyramid mast

---
+++ question35.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+/*
+***************
+******* *******
+******   ******
+*****     *****
+****       ****
+***         ***
+**           **
+*             *
+
+*/
+
+int main() {
+    int n;
+    printf("enter the row:");
+    scanf("%d",&n);
+    int nst=n;
+    int nsp=1;
+    for (int i = 1; i <=2*n+1; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    for (int i = 1; i <=n; i++)
+    {
+        for (int j = 1; j <=nst; j++)
+        {
+            printf("*");
+        }
+        for (int j = 1; j <=nsp; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <=nst; j++)
+        {
+            printf("*");
+        }
+        nst--;
+        nsp+=2;
+        printf("\n");
+        
+
+        
+        
+    }
+    
+    
+    return 0;
+}
